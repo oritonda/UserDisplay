1be6a784fdbc9f5862273d559094a76608ef542e
oritonda <oritonda@thedigitalacademy.co.za>
2021-04-22 01:46:33 +0200

    get all users

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { UsersComponent } from './pages/users/users.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/pages/users/users.component.ts
@@ -0,0 +1,33 @@
+import { UsersApiService } from './../../services/usersApi.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  //declaring user array to store all the users from the api
+  users:User|any
+  //declaring user id to store the user id and initialize with default id if 0
+  userId:number = 0
+
+  constructor(private api:UsersApiService) { }
+
+  ngOnInit(): void {
+    // calling get all user when the component starts
+    this.getAllUsers()
+  }
+  // funtion to get all the user from the database
+  getAllUsers()
+  {
+    // subscribing to the user api service
+    this.api.getUsers().subscribe((users)=>{
+      // initilizing the users array to the data returned by the api
+      this.users = users
+    })
+
+  }
+
+}
