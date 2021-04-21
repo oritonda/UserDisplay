35978804e7706a70c07318e32e6a00c93a19d160
oritonda <oritonda@thedigitalacademy.co.za>
2021-04-21 21:49:22 +0200

    api call

--- frontend/src/app/services/usersApi.service.spec.ts
+++ frontend/src/app/services/usersApi.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UsersApiService } from './users-api.service';
+import { UsersApiService } from './usersApi.service';
 
 describe('UsersApiService', () => {
   let service: UsersApiService;
