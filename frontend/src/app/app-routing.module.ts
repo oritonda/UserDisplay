ac9402613544bcbb398a7f8e44ffc7a09b6f7021
oritonda <oritonda@thedigitalacademy.co.za>
2021-04-21 22:24:32 +0200

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'users',  component:AppComponent},
+  {path:'**',redirectTo:'users',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
